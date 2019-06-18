b763567f2ea042ef8035a4c49c5813cca5154006
alex-janss <alexrjanss@gmail.com>
2019-06-18 16:39:56 -0700

    Added another convolution-pooling layer
    The current CNN architecture is as follows:
    Input -> Conv -> Pool -> Conv -> Pool -> FC
    However, the current implimentation is 2-dimensional,
    i.e. each convolution layer has only one filter.

--- ConvNeuralNet.py
+++ ConvNeuralNet.py
@@ -52,7 +52,7 @@ def inverse_pool(A, nrow = 2, ncol = 2):
 
 def pad(A, width):
     Z = np.zeros((A.shape[0]+2*width,A.shape[1]+2*width))
-    Z[width:A.shape[0]+width,width:A.shape[0]+width] = A
+    Z[width:A.shape[0]+width,width:A.shape[1]+width] = A
     return Z
 
 # shuffles a pair of vectors together,
@@ -65,62 +65,86 @@ def shuffle_together(a, b):
 
 class CNN:
 
-    def __init__(self, num_filters=3):
+    def __init__(self, num_filters=1):
         self.num_filters = num_filters
         self.input_layer = np.zeros((28,28))
         self.padded_input = np.zeros((32,32))
-        self.filtered_layer = np.zeros((self.num_filters,28,28))
-        self.filtered_z = np.zeros((self.num_filters,28,28))
-        self.pooled_layer = np.zeros((self.num_filters,14,14))
-        self.pooled_z = np.zeros((self.num_filters,14,14))
+        self.filtered_layer1 = np.zeros((28,28))
+        self.filtered_z1 = np.zeros((28,28))
+        self.pooled_layer1 = np.zeros((14,14))
+        self.pooled_z1 = np.zeros((14,14))
+        self.padded_pool1 = np.zeros((18,18))
+        self.filtered_layer2 = np.zeros((14,14))
+        self.filtered_z2 = np.zeros((14, 14))
+        self.pooled_layer2 = np.zeros((7, 7))
+        self.pooled_z2 = np.zeros((7, 7))
         self.output_layer = np.zeros((10,1))
         self.output_z = np.zeros((10,1))
 
-        self.filter = np.zeros((self.num_filters,5,5))
-        self.filter_bias = np.zeros(self.num_filters)
-        self.weights = np.zeros((10,self.pooled_layer.size))
+        self.filter1 = np.zeros((5,5))
+        self.filter_bias1 = np.zeros(self.num_filters)
+        self.filter2 = np.zeros((5,5))
+        self.filter_bias2 = np.zeros(self.num_filters)
+        self.weights = np.zeros((10,self.pooled_layer2.size))
         self.biases = np.zeros((10,1))
         self.errors = np.zeros((10,1))
 
 
     def random_init(self):
-        self.filter = np.random.randn(self.num_filters,5,5)
-        self.filter_bias = np.random.randn(self.num_filters)
-        self.weights = np.random.randn(10,self.pooled_layer.size)
-        self.biases = np.random.randn(10,1)
+        self.filter1 = np.random.standard_normal(self.filter1.shape)
+        self.filter_bias1 = np.random.standard_normal(self.filter_bias1.shape)
+        self.filter2 = np.random.standard_normal(self.filter2.shape)
+        self.filter_bias2 = np.random.standard_normal(self.filter_bias2.shape)
+        self.weights = np.random.standard_normal(self.weights.shape)
+        self.biases = np.random.standard_normal(self.biases.shape)
 
     def get_cost(self):
         return la.norm(self.errors)
 
     def add_nets(self, input_net):
-        self.filter += input_net.filter
-        self.filter_bias += input_net.filter_bias
+        self.filter1 += input_net.filter1
+        self.filter_bias1 += input_net.filter_bias1
+        self.filter2 += input_net.filter2
+        self.filter_bias2 += input_net.filter_bias2
         self.weights += input_net.weights
         self.biases += input_net.biases
 
     def scale_net(self, factor):
-        self.filter *= factor
-        self.filter_bias *= factor
+        self.filter1 *= factor
+        self.filter_bias1 *= factor
+        self.filter2 *= factor
+        self.filter_bias2 *= factor
         self.weights *= factor
         self.biases *= factor
         return self
 
 
     def classify(self, input_img, input_label):
+        # input layer
         self.input_layer = np.reshape(input_img, self.input_layer.shape)
-        self.padded_input = pad(self.input_layer,2)
-        for i in range(self.num_filters):
-            self.filtered_z[i] = correlate2d(self.padded_input,self.filter[i],mode="valid") + self.filter_bias[i]
-        self.filtered_layer = sigmoid(self.filtered_z)
-        for i in range(self.num_filters):
-            self.pooled_z[i] = max_pool(self.filtered_z[i])
-        self.pooled_layer = sigmoid(self.pooled_z)
-        self.output_z = (self.weights @ self.pooled_layer.reshape(self.pooled_layer.size,1)) + self.biases
+        self.padded_input = pad(self.input_layer, 2)
+        # filtered layer 1
+        self.filtered_z1 = correlate2d(self.padded_input,self.filter1,mode="valid") + self.filter_bias1
+        self.filtered_layer1 = sigmoid(self.filtered_z1)
+        # pooling 1
+        self.pooled_z1 = max_pool(self.filtered_z1)
+        self.pooled_layer1 = sigmoid(self.pooled_z1)
+        self.padded_pool1 = pad(self.pooled_layer1, 2)
+        # filtered layer 2
+        self.filtered_z2 = correlate2d(self.padded_pool1,self.filter2,mode="valid") + self.filter_bias2
+        self.filtered_layer2 = sigmoid(self.filtered_z2)
+        # pooling 2
+        self.pooled_z2 = max_pool(self.filtered_z2)
+        self.pooled_layer2 = sigmoid(self.pooled_z2)
+
+        # output layer
+        self.output_z = (self.weights @ self.pooled_layer2.reshape(self.pooled_layer2.size,1)) + self.biases
         self.output_layer = sigmoid(self.output_z)
         self.errors = self.output_layer - input_label.reshape(input_label.size,1)
 
 
     def backprop(self):
+        '''
         grad_net = CNN(self.num_filters)
         # errors L3
         grad_net.output_layer = 2 * self.errors * deriv_sigmoid(self.output_z)
@@ -138,6 +162,36 @@ class CNN:
         # filter weights L1
         for i in range(self.num_filters):
             grad_net.filter[i] = correlate2d(self.padded_input, grad_net.filtered_layer[i], mode="valid")
+
+         '''
+        grad_net = CNN(self.num_filters)
+        # error L5
+        grad_net.output_layer = 2 * self.errors * deriv_sigmoid(self.output_z)
+        # errors L4
+        grad_net.pooled_layer2 = np.reshape((self.weights.T @ grad_net.output_layer), self.pooled_layer2.shape) * deriv_sigmoid(self.pooled_z2)
+        # errors L3
+        grad_net.filtered_layer2 = inverse_pool(grad_net.pooled_layer2)
+        # errors L2
+        grad_net.pooled_layer1 = convolve2d(pad(self.filter2, 11), grad_net.filtered_layer2, mode='valid') * deriv_sigmoid(self.pooled_z1)
+        # errors L1
+        grad_net.filtered_layer1 = inverse_pool(grad_net.pooled_layer1)
+
+        # weights
+        # weights L5
+        grad_net.weights = np.outer(grad_net.output_layer, self.pooled_layer2.reshape(self.pooled_layer2.size))
+        # weights L3
+        grad_net.filter2 = correlate2d(self.padded_pool1, grad_net.filtered_layer2, mode='valid')
+        # weights L1
+        grad_net.filter1 = correlate2d(self.padded_input, grad_net.filtered_layer1, mode='valid')
+
+        #biases
+        # biases L5
+        grad_net.biases = grad_net.output_layer
+        # bias L3
+        grad_net.filter_bias2 = np.sum(grad_net.filtered_layer2)
+        # bias L1
+        grad_net.filter_bias1 = np.sum(grad_net.filtered_layer1)
+
         grad_net.scale_net(-1)
         return grad_net
 
@@ -178,6 +232,6 @@ class CNN:
 
 
     def show_filters(self):
-        for i in range(self.filter.shape[0]):
+        for i in range(self.filter1.shape[0]):
             plt.figure()
-            plt.imshow(self.filter[i], cmap="Greys")
+            plt.imshow(self.filter1[i], cmap="Greys")
