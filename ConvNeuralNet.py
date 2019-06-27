ce355a68f71097e76aa299ab1bcb1dc2fb689d63
alex-janss <alexrjanss@gmail.com>
2019-06-27 14:29:44 -0700

    Updated mnist data and random_init, added soft_max and relu functions
    Downloaded most recent image data from mnist. The training set is
    now much larger. Altered the data loading functions to account for
    the different data type of the images. The load_imgs and load_labels
    function now load the data using the gzip functions.
    
    Random_init() now adjusts the randomly initialized weights by
    a factor of 2/sqrt(input_size) as is now standard practice with
    most neural nets. All biases are now initialized to zero, which is
    better for relu neurons.
    
    Soft_max is now applied to the output layer rather than sigmoid.
    This should help with the learning slowdown problem of sigmoid neurons.
    The get_cost function has accordingly been changed to a log-likelyhood
    function and backprop has been altered to calculate the error for
    the softmax layer.
    
    The ReLU function replaced the sigmoid in all other layers. The classify
    and backprop functions now reflect this. This should further improve the
    learning rate.

--- ConvNeuralNet.py
+++ ConvNeuralNet.py
@@ -1,37 +1,37 @@
-import pickle
+import gzip
 import numpy as np
 import numpy.linalg as la
 import scipy.signal as sg
 import matplotlib.pyplot as plt
 
-
-
 np.set_printoptions(linewidth=300)
 image_size = 28  # width and length
-no_of_different_labels = 10  # i.e. 0, 1, 2, 3, ..., 9
+num_train_images = 60000
+num_test_images = 10000
 image_pixels = image_size * image_size
-data_path = "C:/Users/Alex/PycharmProjects/ConvNeuralNet/"
-# saves train and test data to pickle file
-# makes loading faster for future runs
-try:
-    train_data = pickle.load(open("train_data.pickle", "rb"))
-    test_data = pickle.load(open("test_data.pickle", "rb"))
-except (OSError, IOError) as e:
-    train_data = np.loadtxt(data_path + "mnist_train.csv", delimiter=",")
-    test_data = np.loadtxt(data_path + "mnist_test.csv", delimiter=",")
-    pickle.dump(train_data, open("train_data.pickle", "wb"))
-    pickle.dump(test_data, open("test_data.pickle", "wb"))
+data_path = ""  # set path to image data if needed
 
-fac = .99 / 255
-train_imgs = np.asfarray(train_data[:, 1:]) * fac + .01
-test_imgs = np.asfarray(test_data[:, 1:]) * fac + .01
-train_labels = np.asfarray(train_data[:, :1])
-test_labels = np.asfarray(test_data[:, :1])
 
-lr = np.arange(no_of_different_labels)
-# transform labels into one hot representation
-train_labels = (lr == train_labels).astype(np.float)
-test_labels = (lr == test_labels).astype(np.float)
+def load_imgs(filename, num_imgs, image_size):
+    f = gzip.open(filename, 'r')
+    f.read(16)
+    buf = f.read(image_size * image_size * num_imgs)
+    imgs = (np.frombuffer(buf, dtype=np.uint8).astype(np.float32)).reshape(num_imgs, image_size**2)
+    return imgs * (.99 / 255) + .01  # convert each pixel value into range [.01,1]
+
+def load_labels(filename, num_labels):
+    f = gzip.open(filename, 'r')
+    f.read(8)
+    buf = f.read(num_labels)
+    labels = (np.frombuffer(buf, dtype=np.uint8).astype(np.int64)).reshape(num_labels, 1)
+    return (np.arange(10) == labels).astype(np.float) # transform into one-hot representation
+
+
+# loading the data
+train_imgs = load_imgs(data_path + 'train-images-idx3-ubyte.gz', num_train_images, image_size)
+test_imgs = load_imgs(data_path + 't10k-images-idx3-ubyte.gz', num_test_images, image_size)
+train_labels = load_labels(data_path + 'train-labels-idx1-ubyte.gz', num_train_images)
+test_labels = load_labels(data_path + 't10k-labels-idx1-ubyte.gz', num_test_images)
 
 
 def sigmoid(input_vector):
@@ -40,6 +40,16 @@ def sigmoid(input_vector):
 
 def deriv_sigmoid(input_vector):
     return 1. / (np.pi * (1 + input_vector ** 2))
+
+
+def soft_max(input_vector):
+    return np.exp(input_vector) / np.sum(np.exp(input_vector))
+
+
+def dropout(A, factor):
+    Z = np.random.random_sample(A.shape)
+    A[Z<factor] = 0.
+    A *= 1/(1-factor)
 
 
 def max_pool(A, nrow = 2, ncol = 2):
@@ -66,6 +76,7 @@ def shuffle_together(a, b):
     np.random.set_state(rng_state)
     np.random.shuffle(b)
 
+
 class CNN:
 
     def __init__(self, num_filters=3):
@@ -73,37 +84,33 @@ class CNN:
         self.input_layer = np.zeros((28,28))
         self.padded_input = np.zeros((32,32))
         self.filtered_layer1 = np.zeros((num_filters,28,28))
-        self.filtered_z1 = np.zeros((num_filters,28,28))
         self.pooled_layer1 = np.zeros((num_filters,14,14))
-        self.pooled_z1 = np.zeros((num_filters,14,14))
         self.padded_pool1 = np.zeros((num_filters,18,18))
         self.filtered_layer2 = np.zeros((num_filters,14,14))
-        self.filtered_z2 = np.zeros((num_filters,14, 14))
         self.pooled_layer2 = np.zeros((num_filters,7, 7))
-        self.pooled_z2 = np.zeros((num_filters,7, 7))
         self.output_layer = np.zeros((10,1))
-        self.output_z = np.zeros((10,1))
 
         self.filter1 = np.zeros((num_filters,5,5))
         self.filter_bias1 = np.zeros((self.num_filters,1,1))
         self.filter2 = np.zeros((num_filters,num_filters,5,5))
         self.filter_bias2 = np.zeros((self.num_filters,1,1))
-        self.weights = np.zeros((10,self.pooled_layer2.size))
+        self.weights = np.zeros((10, self.pooled_layer2.size))
         self.biases = np.zeros((10,1))
-        self.errors = np.zeros((10,1))
+        self.label = np.zeros((10,1))
 
 
-
+    # each weight is adjusted by 2 / sqrt(input size)
     def random_init(self):
-        self.filter1 = np.random.standard_normal(self.filter1.shape)
-        self.filter_bias1 = np.random.standard_normal(self.filter_bias1.shape)
-        self.filter2 = np.random.standard_normal(self.filter2.shape)
-        self.filter_bias2 = np.random.standard_normal(self.filter_bias2.shape)
-        self.weights = np.random.standard_normal(self.weights.shape)
-        self.biases = np.random.standard_normal(self.biases.shape)
+        self.filter1 = np.random.standard_normal(self.filter1.shape)*np.sqrt(2/self.filter1[0].size)
+        self.filter_bias1 = np.zeros(self.filter_bias1.shape)
+        self.filter2 = np.random.standard_normal(self.filter2.shape)*np.sqrt(2/self.filter2[0].size)
+        self.filter_bias2 = np.zeros(self.filter_bias2.shape)
+        self.weights = np.random.standard_normal(self.weights.shape)*np.sqrt(2/self.weights.shape[-1])
+        self.biases = np.zeros(self.biases.shape)
 
     def get_cost(self):
-        return la.norm(self.errors)
+        return -np.log(self.output_layer[np.argmax(self.label)])
+
 
     def add_nets(self, input_net):
         self.filter1 += input_net.filter1
@@ -128,54 +135,34 @@ class CNN:
         self.input_layer = np.reshape(input_img, self.input_layer.shape)
         self.padded_input = pad(self.input_layer, 2)
         # filtered layer 1
-        self.filtered_z1 = np.array([sg.correlate(self.padded_input,filt,mode="valid") for filt in self.filter1]) + self.filter_bias1
-        self.filtered_layer1 = sigmoid(self.filtered_z1)
+        self.filtered_layer1 = np.array([sg.correlate(self.padded_input,filt,mode="valid") for filt in self.filter1]) + self.filter_bias1
+        self.filtered_layer1[self.filtered_layer1<0] = 0. # RELU
         # pooling 1
-        self.pooled_z1 = max_pool(self.filtered_z1)
-        self.pooled_layer1 = sigmoid(self.pooled_z1)
+        self.pooled_layer1 = max_pool(self.filtered_layer1)
         self.padded_pool1 = pad(self.pooled_layer1, 2)
         # filtered layer 2
-        self.filtered_z2 = np.reshape(np.array([sg.correlate(self.padded_pool1, filt,mode="valid") for filt in self.filter2]),self.filtered_z2.shape) + self.filter_bias2
-        self.filtered_layer2 = sigmoid(self.filtered_z2)
+        self.filtered_layer2 = np.reshape(np.array([sg.correlate(self.padded_pool1, filt,mode="valid") for filt in self.filter2]),self.filtered_layer2.shape) + self.filter_bias2
+        self.filtered_layer2[self.filtered_layer2<0] = 0. # RELU
         # pooling 2
-        self.pooled_z2 = max_pool(self.filtered_z2)
-        self.pooled_layer2 = sigmoid(self.pooled_z2)
+        self.pooled_layer2 = max_pool(self.filtered_layer2)
         # output layer
-        self.output_z = (self.weights @ self.pooled_layer2.reshape(self.pooled_layer2.size,1)) + self.biases
-        self.output_layer = sigmoid(self.output_z)
-        self.errors = self.output_layer - input_label.reshape(input_label.size,1)
+        self.output_layer = soft_max((self.weights @ self.pooled_layer2.reshape(self.pooled_layer2.size,1)) + self.biases)
+        self.label = input_label.reshape(input_label.size,1)
+
 
 
     def backprop(self):
-        '''
-        grad_net = CNN(self.num_filters)
-        # errors L3
-        grad_net.output_layer = 2 * self.errors * deriv_sigmoid(self.output_z)
-        # biases L3
-        grad_net.biases = grad_net.output_layer
-        # weights L3
-        grad_net.weights = np.outer(grad_net.output_layer, self.pooled_layer.reshape(self.pooled_layer.size))
-        # errors L2
-        grad_net.pooled_layer = np.reshape((self.weights.T @ grad_net.output_layer), self.pooled_layer.shape) * deriv_sigmoid(self.pooled_z)
-        # errors L1
-        for i in range(self.num_filters):
-            grad_net.filtered_layer[i] = inverse_pool(grad_net.pooled_layer[i])
-        # bias L1
-        grad_net.filter_bias = np.sum(grad_net.filtered_layer, axis=(1,2))
-        # filter weights L1
-        for i in range(self.num_filters):
-            grad_net.filter[i] = correlate2d(self.padded_input, grad_net.filtered_layer[i], mode="valid")
-
-         '''
         grad_net = CNN(self.num_filters)
         # error L5
-        grad_net.output_layer = 2 * self.errors * deriv_sigmoid(self.output_z)
+        grad_net.output_layer = self.output_layer - self.label
         # errors L4
-        grad_net.pooled_layer2 = np.reshape((self.weights.T @ grad_net.output_layer), self.pooled_layer2.shape) * deriv_sigmoid(self.pooled_z2)
+        grad_net.pooled_layer2 = np.reshape((self.weights.T @ grad_net.output_layer), self.pooled_layer2.shape)
+        grad_net.pooled_layer2[self.pooled_layer2<=0] = 0.  # backprop through RELU
         # errors L3
         grad_net.filtered_layer2 = inverse_pool(grad_net.pooled_layer2)
         # errors L2
-        grad_net.pooled_layer1 = np.reshape(np.array([sg.convolve(pad(filt, 11), grad_net.filtered_layer2, mode='valid') for filt in self.filter2]), self.pooled_layer1.shape) * deriv_sigmoid(self.pooled_z1)
+        grad_net.pooled_layer1 = np.reshape(np.array([sg.convolve(pad(filt, 11)[...,::-1,::-1], grad_net.filtered_layer2, mode='valid') for filt in self.filter2]), self.pooled_layer1.shape)
+        grad_net.pooled_layer1[self.pooled_layer1<=0] = 0.  # backprop through RELU
         # errors L1
         grad_net.filtered_layer1 = inverse_pool(grad_net.pooled_layer1)
 
@@ -183,13 +170,12 @@ class CNN:
         # weights L5
         grad_net.weights = np.outer(grad_net.output_layer, self.pooled_layer2.reshape(self.pooled_layer2.size))
         # weights L3
-        for i in range(grad_net.filtered_layer2.shape[0]):
-            for j in range(self.padded_pool1.shape[0]):
+        for i in np.arange(grad_net.filtered_layer2.shape[0]):
+            for j in np.arange(self.padded_pool1.shape[0]):
                 grad_net.filter2[i][j] = sg.correlate(self.padded_pool1[j], grad_net.filtered_layer2[i],mode='valid')
         # weights L1
-        for i in range(grad_net.filtered_layer1.shape[0]):
+        for i in np.arange(grad_net.filtered_layer1.shape[0]):
             grad_net.filter1[i] = sg.correlate(self.padded_input, grad_net.filtered_layer1[i], mode='valid')
-
         # biases
         # biases L5
         grad_net.biases = grad_net.output_layer
@@ -202,42 +188,49 @@ class CNN:
         return grad_net
 
 
-    def train(self, batch_size=1, learn_rate=1, epochs=5, init=True):
+
+    def train(self, epochs, learn_rate=.07, batch_size=10, init=True):
         if init:
             self.random_init()
         for k in range(epochs):
             shuffle_together(train_imgs, train_labels)
             avg_cost = 0.0
             # divide the training set into (10000/batch_size) batches
-            for i in range(10000 // batch_size):
-                # classify a batch of images
-                self.classify(train_imgs[i * batch_size:i * batch_size + batch_size], train_labels[i * batch_size:i * batch_size + batch_size])
-                # calculate the average cost for the batch
-                avg_cost += self.get_cost()
-                # calculate the gradient vector via backprop(),
+            for i in range(train_imgs.shape[0] // batch_size):
+                grad = CNN(self.num_filters)
+                for j in range(batch_size):
+                    # classify a batch of images
+                    self.classify(train_imgs[i*batch_size+j], train_labels[i*batch_size+j])
+                    # calculate the average cost for the batch
+                    avg_cost += self.get_cost()
+                    # calculate the gradient vector via backprop()
+                    grad.add_nets(self.backprop())
                 # then scale the gradient by learn_rate and add to the net
-                self.add_nets(self.backprop().scale_net(learn_rate))
+                self.add_nets(grad.scale_net(learn_rate/batch_size))
             # average over the number of batches
-            avg_cost /= (10000/batch_size)
+            avg_cost /= (train_imgs.shape[0])
             print('%.5f' % avg_cost, "\tepoch", k+1)
+
 
 
     def test(self):
         avg_cost = 0.0
         cm = np.zeros((10, 10), dtype=int)
-        for i in range(10000):
+        for i in range(test_imgs.shape[0]):
             self.classify(test_imgs[i], test_labels[i])
             avg_cost += self.get_cost()
             cm[np.argmax(test_labels[i]), np.argmax(self.output_layer)] += 1
-        avg_cost /= 10000
-        accuracy = cm.trace() / 10000
+        avg_cost /= test_imgs.shape[0]
+        accuracy = cm.trace() / test_imgs.shape[0]
         print("Confusion Matrix:")
         print(cm)
         print("average cost: ", avg_cost)
         print("accuracy: ", accuracy * 100, "%")
 
 
+
     def show_filters(self):
         for i in range(self.filter1.shape[0]):
             plt.figure()
             plt.imshow(self.filter1[i], cmap="Greys")
+
